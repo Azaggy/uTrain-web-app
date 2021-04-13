c0c5cf36b63fd978ae2364c7b2c7e5c82632a72f
The Rebellion Queen <azaggy1@outlook.com>
2021-04-13 18:49:08 -0500

    Added Hello, World

---
+++ hello-world/src/test/java/org/launchcode/helloworld/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
