8d6e39167286cbe16d220e0e71a36191208a1003
Matt R <mattreimer1@yahoo.com>
2021-04-13 19:25:10 -0500

    mer branch first commit

---
+++ src/test/java/org/launchcode/Utrain/UtrainApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.Utrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
