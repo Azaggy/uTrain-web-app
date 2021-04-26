68a4dbc8d18284ab934d5d4bb3989ff858c36035
andrestafford87/github.com <andrestafford87@gmail.com>
2021-04-26 09:28:11 -0500

    created search box, not yet complete

---
+++ src/main/resources/static/main.js
@@ -0,0 +1,8 @@
+const selected = document.querySelector(".selected");
+const optionsContainer = document.querySelector(".option-container");
+
+ const optionsList = document.querySelectorAll(".option");
+
+selected.addEventListener("click", () => {
+    optionsContainer.classList.toggle("active");
+})
