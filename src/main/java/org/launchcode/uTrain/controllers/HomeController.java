7454185b893d18ed82595cd6ea69389c00323e18
Jay Steinle <jay.steinle62@gmail.com>
2021-04-26 18:58:29 -0500

    Add register form

--- src/main/java/org/launchcode/uTrain/controllers/HomeController.java
+++ src/main/java/org/launchcode/uTrain/controllers/HomeController.java
@@ -2,6 +2,7 @@ package org.launchcode.uTrain.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -11,5 +12,11 @@ public class HomeController {
     @GetMapping("")
     public String hello() {
         return "index";
+    }
+
+    @GetMapping("/register")
+    public String displayRegisterForm(Model model){
+        model.addAttribute("title", "Register");
+        return "register";
     }
 }
