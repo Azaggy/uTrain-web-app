0d8aa7981c8125dc437318f5170d3ddc8dbd972c
Jay Steinle <jay.steinle62@gmail.com>
2021-04-30 18:02:02 -0500

    Add Message

---
+++ src/main/java/org/launchcode/uTrain/controllers/MessageController.java
@@ -0,0 +1,82 @@
+package org.launchcode.uTrain.controllers;
+
+import org.launchcode.uTrain.data.MessageRepository;
+import org.launchcode.uTrain.data.UserRepository;
+import org.launchcode.uTrain.models.Message;
+import org.launchcode.uTrain.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+import java.util.Optional;
+
+@Controller
+public class MessageController {
+
+        private static final String userSessionKey = "user";
+
+        public User getUserFromSession(HttpSession session) {
+            Integer userId = (Integer) session.getAttribute(userSessionKey);
+            if (userId == null) {
+                return null;
+            }
+            Optional<User> user = userRepository.findById(userId);
+            if (user.isEmpty()) {
+                return null;
+            }
+            return  user.get();
+        }
+
+        @Autowired
+        UserRepository userRepository;
+
+        @Autowired
+        MessageRepository messageRepository;
+
+
+        @GetMapping("message/addmessage")
+        public String displayAddMessage(Model model, HttpServletRequest request){
+
+            User user = (User) getUserFromSession(request.getSession());
+
+            model.addAttribute("user", user);
+            model.addAttribute("loggedIn", true);
+            model.addAttribute("title", "Add Message");
+            model.addAttribute(new Message());
+            return "message/addmessage";
+        }
+
+        @PostMapping("message/addmessage")
+        public String processAddMessage(@ModelAttribute @Valid Message newMessage, Errors errors, Model model){
+
+            if(errors.hasErrors()) {
+                model.addAttribute("title", "Add Message");
+                return "message/addmessage";
+            }
+
+            MessageRepository.save(newMessage);
+            return "redirect:index";
+        }
+
+        @GetMapping("message/index")
+        public String messageIndex(Model model, HttpServletRequest request){
+
+            User user = (User) getUserFromSession(request.getSession());
+
+            model.addAttribute("user", user);
+            model.addAttribute("loggedIn", true);
+            model.addAttribute("messages", MessageRepository.findAll());
+            model.addAttribute("title", "Message List");
+
+            return "message/index";
+        }
+
+
+    }
---
+++ src/main/java/org/launchcode/uTrain/models/Message.java
@@ -0,0 +1,69 @@
+package org.launchcode.uTrain.models;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+@Entity
+public class Message extends AbstractEntity{
+
+
+    @NotNull
+    @Size(max = 50)
+    private String body;
+
+    @NotNull
+    private String recipient;
+
+    @ManyToMany
+    @NotNull
+    private String sender;
+
+    Date date = new Date();
+
+    public Message() {
+    }
+
+    public Message(@NotNull @Size(max = 50) String body, @NotNull String recipient, @NotNull String sender, Date date) {
+        this.body = body;
+        this.recipient = recipient;
+        this.sender = sender;
+        this.date = date;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
