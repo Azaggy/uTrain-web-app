399127c9514782ca8efc5f37108033d1cff36d6e
Jeremy G <jgathin@gmail.com>
2021-04-29 21:02:18 -0500

    edited a character in workout controller

--- src/main/java/org/launchcode/uTrain/controllers/WorkOutController.java
+++ src/main/java/org/launchcode/uTrain/controllers/WorkOutController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class WorkOutController {
 
     @GetMapping("index")
-    public String indexSearch(Model moel){
+    public String indexSearch(Model model){
         return("exercise/index");
     }
 
