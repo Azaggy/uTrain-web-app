770801cc18aa1b3935d97ab7223e95930a1aaadc
The Rebellion Queen <azaggy1@outlook.com>
2021-05-04 21:13:41 -0500

    almost there

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,20 +1,20 @@
-function sayHello() {
-   alert("Hello World")
-}
-
-function conversionUKToUS(int weight, int height) {
-    let kg = 2.20
-    let m = 39.37
-    let newHeight = height/12
-    let newWeight = weight/kg
-    let height2 = height/m
-    console.log(newWeight/(height2**2))
-}
-
-let modal = document.getElementById('id01');
-
-window.onclick = function(event) {
-    if (event.target == model) {
-        modal.style.display = "none";
-    }
-}
+//function sayHello() {
+//   alert("Hello World")
+//}
+//
+//function conversionUKToUS(int weight, int height) {
+//    let kg = 2.20
+//    let m = 39.37
+//    let newHeight = height/12
+//    let newWeight = weight/kg
+//    let height2 = height/m
+//    console.log(newWeight/(height2**2))
+//}
+//
+//let modal = document.getElementById('id01');
+//
+//window.onclick = function(event) {
+//    if (event.target == model) {
+//        modal.style.display = "none";
+//    }
+//}
