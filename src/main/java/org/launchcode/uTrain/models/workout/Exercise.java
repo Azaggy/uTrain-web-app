95e0fff4dea3c433075987e8ef2571578248d389
JeremyG <jgathin@gmail.com>
2021-05-07 21:11:04 -0500

    added workout and activity classes

--- src/main/java/org/launchcode/uTrain/models/workout/Exercise.java
+++ src/main/java/org/launchcode/uTrain/models/workout/Exercise.java
@@ -1,4 +1,36 @@
 package org.launchcode.uTrain.models.workout;
 
-public class Exercise {
+import org.launchcode.uTrain.models.AbstractEntity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Exercise extends AbstractEntity {
+
+    private String name;
+
+    private ExerciseType exerciseType;
+
+    public Exercise () {}
+
+    public Exercise (String name, ExerciseType exerciseType) {
+        this.name = name;
+        this.exerciseType = exerciseType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ExerciseType getExerciseType() {
+        return exerciseType;
+    }
+
+    public void setExerciseType(ExerciseType exerciseType) {
+        this.exerciseType = exerciseType;
+    }
 }
