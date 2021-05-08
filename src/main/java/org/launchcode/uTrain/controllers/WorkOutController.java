0e2c9cf4a6dcc77376b4f3d769255a0662d48763
igentry0630 <74032600+igentry0630@users.noreply.github.com>
2021-05-07 19:18:58 -0500

    Upgraded Workout Controller

--- src/main/java/org/launchcode/uTrain/controllers/WorkOutController.java
+++ src/main/java/org/launchcode/uTrain/controllers/WorkOutController.java
@@ -3,20 +3,34 @@ package org.launchcode.uTrain.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("exercise")
 public class WorkOutController {
 
+    private static List<String> customWorkouts = new ArrayList<>();
+
     @GetMapping("index")
-    public String indexSearch(Model moel){
+    public String indexSearch(Model model){
+        model.addAttribute("customWorkouts", customWorkouts);
         return("exercise/index");
     }
 
     @GetMapping("workOut")
     public String trainersDisplay(){
         return("exercise/workOut");
+    }
+
+    @PostMapping("index")
+    public String createWorkOutForm(@RequestParam String workOutName) {
+        customWorkouts.add(workOutName);
+        return "exercise/index";
     }
 
 
