e5e71e02bde2093d14d9b407045aefd826dfc805
igentry0630 <74032600+igentry0630@users.noreply.github.com>
2021-05-16 00:35:05 -0500

    good progress, time to create custom link

---
+++ src/main/java/org/launchcode/uTrain/controllers/CustomWorkoutController.java
@@ -0,0 +1,58 @@
+package org.launchcode.uTrain.controllers;
+
+import org.launchcode.uTrain.data.WorkoutData;
+import org.launchcode.uTrain.models.workout.Workout;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+
+
+@Controller
+@RequestMapping("customworkout")
+public class CustomWorkoutController {
+
+
+    @GetMapping
+    public String displayAllExercises(Model model) {
+        model.addAttribute("customWorkouts", WorkoutData.getAll());
+        return "customworkout/index";
+    }
+
+    @GetMapping("create")
+    public String renderCreateWorkoutForm() {
+        return "customworkout/create";
+    }
+
+    @PostMapping("create")
+    public String createWorkout(@ModelAttribute Workout newWorkout,
+                                Errors errors, Model model) {
+        if(errors.hasErrors()) {
+            model.addAttribute("title", "Create Workout");
+            return "customworkout/create";
+        }
+        WorkoutData.add(newWorkout);
+        return "redirect:";
+    }
+
+    @GetMapping("delete")
+    public String displayDeleteWorkoutForm(Model model) {
+        model.addAttribute("title", "Delete Workouts");
+        model.addAttribute("workouts", WorkoutData.getAll());
+        return "customworkout/delete";
+    }
+
+    @PostMapping("delete")
+    public String processDeleteWorkoutForm(@RequestParam(required = false) int[] workoutIds) {
+
+        if (workoutIds != null) {
+            for (int id : workoutIds) {
+                WorkoutData.remove(id);
+            }
+        }
+        return "redirect:";
+    }
+}
+
+
---
+++ src/main/java/org/launchcode/uTrain/data/WorkoutData.java
@@ -0,0 +1,32 @@
+package org.launchcode.uTrain.data;
+
+import org.launchcode.uTrain.models.workout.Workout;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class WorkoutData {
+    //    need a place to put workout
+    private static Map<Integer, Workout> workouts = new HashMap<>();
+
+    //    get all workout
+    public static Collection<Workout> getAll() {
+        return workouts.values();
+    }
+
+
+    //    get a single workout
+    public static Workout getById(int id) {
+        return workouts.get(id);
+    }
+    //    add a workout
+    public static void add(Workout workout) {
+        workouts.put(workout.getId(), workout);
+    }
+
+    //    remove an workout
+    public static void remove(int id) {
+        workouts.remove(id);
+    }
+}
