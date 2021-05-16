d13195dccdb9c12efbf0ea4d1cce02c213fbc51c
igentry0630 <74032600+igentry0630@users.noreply.github.com>
2021-05-15 23:34:36 -0500

    templates added, more work on the controller needed

---
+++ src/main/java/org/launchcode/uTrain/models/workout/CustomWorkout.java
@@ -0,0 +1,36 @@
+package org.launchcode.uTrain.models.workout;
+
+import javax.validation.constraints.Size;
+
+public class CustomWorkout {
+    private int id;
+    private static int nextId = 1;
+
+    @Size(min = 3, max = 30, message = "Workout must be between 3 and 50 characters")
+    private String name;
+
+
+    public CustomWorkout(String name) {
+        this.name = name;
+        this.id = nextId;
+        nextId++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
+
