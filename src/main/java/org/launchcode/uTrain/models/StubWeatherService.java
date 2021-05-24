5e85fb2c1e2260bc62e83b16659ba6356b15b65c
Matt R <mattreimer1@yahoo.com>
2021-05-23 21:57:39 -0500

    weather api

---
+++ src/main/java/org/launchcode/uTrain/models/StubWeatherService.java
@@ -0,0 +1,13 @@
+package org.launchcode.uTrain.models;
+
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class StubWeatherService {
+
+    public CurrentWeather getCurrentWeather(String city, String country) {
+        return new CurrentWeather("Clear", BigDecimal.ONE, BigDecimal.ZERO, BigDecimal.TEN);
+    }
+}
